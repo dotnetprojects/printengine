439f04782ef20e4ba3b69341e68e67a9103b36de
kraov <krasnyh-oleg@yandex.ru>
2017-03-07 10:59:01 +0400

    Классы для описания таблицы

---
+++ DotNetProjects.PrintEngine/Utils/CustomGridData/CustomGridDataColumn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace SUT.PrintEngine.Utils
+{
+    public class CustomGridDataColumn
+    {
+        public object Header { get; set; }
+
+        public double Width { get; set; }
+
+        public Func<object, double, UIElement> CellFunc { get; set; } 
+
+        public object GridColumn { get; set; }
+    }
+}
---
+++ DotNetProjects.PrintEngine/Utils/CustomGridData/CustomGridDataTable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace SUT.PrintEngine.Utils
+{
+    public class CustomGridDataTable
+    {
+        public double ScaleWidths { get; set; } = 1;
+        public IList<CustomGridDataColumn> Columns { get; } = new List<CustomGridDataColumn>();
+        public IList<CustomGridDataRow> Rows { get; } = new List<CustomGridDataRow>(); 
+        public Thickness Margin { get; set; }
+        public IList<double> ColumnWidths
+        {
+            get { return Columns.Select(c => c.Width * ScaleWidths).ToList(); }
+        }
+
+        public IList<double> ColumnWidthsWithoutScale
+        {
+            get { return Columns.Select(c => c.Width).ToList(); }
+        }
+
+        public string[] ColumnNames
+        {
+            get { return Columns.Select(c => c.Header.ToString()).ToArray(); }
+        } 
+
+        public void AddColumn(CustomGridDataColumn column)
+        {
+            Columns.Add(column);
+        }
+
+        public void AddRow(CustomGridDataRow row)
+        {
+            Rows.Add(row);
+        }
+    }
+}
