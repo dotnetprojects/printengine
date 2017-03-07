30eb4d6692c0f4a745d965d5cc156be3534d4c4b
kraov <krasnyh-oleg@yandex.ru>
2017-03-07 11:00:28 +0400

    Вью-модели для печати таблицы

---
+++ DotNetProjects.PrintEngine/Paginators/CustomGridDataPaginator.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using SUT.PrintEngine.Paginators;
+using SUT.PrintEngine.Utils;
+
+namespace DotNetProjects.PrintEngine.Paginators
+{
+    public class CustomGridDataPaginator: ItemsPaginator
+    {
+        public CustomGridDataPaginator(DrawingVisual source, Size printSize, Thickness pageMargins, PrintTableDefination printTableDefination) : 
+            base(source, printSize, pageMargins, printTableDefination)
+        {
+        }
+    }
+}
---
+++ DotNetProjects.PrintEngine/ViewModels/CustomGridDataPrintContolViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using DotNetProjects.PrintEngine.Paginators;
+using DotNetProjects.PrintEngine.Utils;
+using SUT.PrintEngine.Utils;
+using SUT.PrintEngine.Views;
+
+namespace SUT.PrintEngine.ViewModels
+{
+    public class CustomGridDataPrintContolViewModel: ItemsPrintControlViewModel, IDataTablePrintControlViewModel
+    {
+        private readonly CustomGridDataTable _table;
+        private readonly string _header;
+        public CustomGridDataPrintContolViewModel(PrintControlView view, CustomGridDataTable table, string header) : base(view)
+        {
+            _table = table;
+            _header = header;
+            PrintControlView.ScaleButtonVisibility(false);
+        }
+
+        protected override void CreatePaginator(DrawingVisual visual, Size printSize)
+        {
+            Paginator = new CustomGridDataPaginator(visual, printSize, PrintUtility.GetPageMargin(CurrentPrinterName), PrintTableDefination);
+        }
+
+        public override void ReloadPreview()
+        {
+            CustomGridDataHelper.SetupPrintControlPresenter(_table, this, _header, CurrentPaper, PageOrientation);
+            FitToPage = false;
+            base.ReloadPreview();
+        }
+    }
+}
